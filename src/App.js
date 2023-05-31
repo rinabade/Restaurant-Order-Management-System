a54a1214b098e9bd6cb350a1238db2b383c26074
aditistha <sthaaditi1@gmail.com>
2023-05-31 23:24:21 +0545

    pages Route changes

--- src/App.js
+++ src/App.js
@@ -13,6 +13,13 @@ import Employees from './pages/Admin/Employees/Employees';
 import AdminRegister from './pages/Admin/AdminRegister/AdminRegister';
 import AdminMenu from './pages/Admin/AdminMenu/AdminMenu';
 import Category from './pages/Admin/Category/Category';
+import Orders from './pages/Admin/Orders/Orders';
+import Inventory from './pages/Admin/Inventory/Inventory';
+import Feedback from './pages/Admin/Feedback/Feedback';
+import Payment from './pages/Admin/Payment/Payment';
+import Suppliers_payment from './pages/Admin/Suppliers_payment/Suppliers_payment';
+import Admin_profile from './pages/Admin/Admin_profile/Admin_profile';
+import Suppliers from './pages/Admin/Suppliers/Suppliers';
 
 function App() {
   return (
@@ -29,6 +36,13 @@ function App() {
             <Route path="Employees" element={<Employees />} />
             <Route path="AdminRegister" element={<AdminRegister />} />
             <Route path="AdminMenu" element={<AdminMenu />} />
+            <Route path="Orders" element={<Orders/>} />
+            <Route path="Inventory" element={<Inventory/>} />
+            <Route path="Feedback" element={<Feedback/>} />
+            <Route path="Payment" element={<Payment/>} />
+            <Route path="Suppliers_payment" element={<Suppliers_payment/>} />
+            <Route path="Admin_profile" element={<Admin_profile/>} />
+            <Route path="Suppliers" element={<Suppliers/>} />
             <Route path="Category" element={<Category/>} />
 
           </Route>
---
+++ src/pages/Admin/Payment/Payment.js
@@ -0,0 +1,63 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import '../Table.css'
+
+function createData(name, ID, Category, Price, Action) {
+  return { name, ID, Category, Price, Action };
+}
+
+const rows = [
+  createData("Veg", 18908424, "momo", "200"),
+  createData("Chicken", 18908424, "momo", "200"),
+  createData("buff", 18908424, "momo", "200"),
+  createData("buff", 18908421, "momo", "200"),
+];
+
+
+export default function BasicTable() {
+  return (
+      <div className="Table">
+      <h3>Payments</h3>
+        <TableContainer
+          component={Paper}
+          style={{ boxShadow: "0px 13px 20px 0px #80808029" }}
+        >
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell align="left">ID</TableCell>
+                <TableCell align="left">Category</TableCell>
+                <TableCell align="left">Price</TableCell>
+                <TableCell align="left">Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody style={{ color: "white" }}>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="left">{row.ID}</TableCell>
+                  <TableCell align="left">{row.Category}</TableCell>
+                  <TableCell align="left">
+                    <span className="status">{row.Price}</span>
+                  </TableCell>
+                  <TableCell align="left" className="Details">Delete</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+  );
+}
