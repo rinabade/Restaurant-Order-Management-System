aa53cf94efea140bf754e62e39e667a5a7c315e1
aditistha <sthaaditi1@gmail.com>
2023-06-29 17:25:14 +0545

    changes in cashier table

--- src/pages/Cashier/CashierOrder/CashierOrder.js
+++ src/pages/Cashier/CashierOrder/CashierOrder.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -22,19 +22,19 @@ import {
   getAllMenu,
 } from "../../../api/userAction";
 
-function createData(OrderItems ,Quantity) {
-  return {  OrderItems ,Quantity };
+function createData(OrderItems, Quantity, Price) {
+  return { OrderItems, Quantity, Price };
 }
 
 const rows = [
-  createData("Veg momo", 2),
-  createData("Chicken momo", 1),
-  
+  createData("Veg momo", 2, 5),
+  createData("Chicken momo", 1, 8),
 ];
 
 export default function CashierOrder() {
   const [selectedTable, setSelectedTable] = useState("");
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [paymentMethod, setPaymentMethod] = useState("");
 
   // Function to handle button click
   const handleTableButtonClick = (tableNumber) => {
@@ -47,48 +47,96 @@ export default function CashierOrder() {
     setIsDialogOpen(false);
   };
 
+  // Function to handle redirect
+  const handleRedirect = () => {
+    setIsDialogOpen(false);
+    window.location.href = '/fonepay';
+  };
+
+  // Function to handle payment method change
+  const handlePaymentMethodChange = (event) => {
+    setPaymentMethod(event.target.value);
+  };
+
+  // Function to calculate total price
+  const calculateTotalPrice = () => {
+    let totalPrice = 0;
+    for (const row of rows) {
+      totalPrice += row.Quantity * row.Price;
+    }
+    return totalPrice;
+  };
+
   return (
     <div>
       {/* Buttons for each table */}
-      <Button className="CashButton" onClick={() => handleTableButtonClick(1)}>Table 1</Button>
-      <Button className="CashButton" onClick={() => handleTableButtonClick(2)}>Table 2</Button>
-      <Button className="CashButton" onClick={() => handleTableButtonClick(3)}>Table 3</Button>
+      <Button
+        className="CashButton"
+        onClick={() => handleTableButtonClick(1)}
+      >
+        Table 1
+      </Button>
+      <Button
+        className="CashButton"
+        onClick={() => handleTableButtonClick(2)}
+      >
+        Table 2
+      </Button>
+      <Button
+        className="CashButton"
+        onClick={() => handleTableButtonClick(3)}
+      >
+        Table 3
+      </Button>
 
       {/* Pop-up dialog */}
       <Dialog open={isDialogOpen} onClose={handleDialogClose}>
         <DialogTitle>Table {selectedTable}</DialogTitle>
         <DialogContent>
-        <TableContainer
-          component={Paper}
-          style={{ boxShadow: "0px 13px 20px 0px #80808029" }}
-        >
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Order Items</TableCell>
-                <TableCell align="left">Quantity</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody style={{ color: "white" }}>
-              {rows.map((row) => (
-                <TableRow
-                  key={row.name}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.OrderItems}
-                  </TableCell>
-                  <TableCell align="left">{row.Quantity}</TableCell>
-                 
+          <TableContainer
+            component={Paper}
+            style={{ boxShadow: "0px 13px 20px 0px #80808029" }}
+          >
+            <Table sx={{ minWidth: 650 }} aria-label="simple table" className="CashierOrderTable">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Order Items</TableCell>
+                  <TableCell align="left">Quantity</TableCell>
+                  <TableCell align="left">Price</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody style={{ color: "white" }}>
+                {rows.map((row, index) => (
+                  <TableRow
+                    key={index}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.OrderItems}
+                    </TableCell>
+                    <TableCell align="left">{row.Quantity}</TableCell>
+                    <TableCell align="left">{row.Price}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <p className="CashierOrderTotal">
+            <span>Total Price: </span>
+            <span>Rs.{calculateTotalPrice()}</span>
+          </p>
+          <div className="PaymentMethodContainer">
+            <span>Select Payment Method:</span>
+            <select value={paymentMethod} onChange={handlePaymentMethodChange}>
+              <option value="">Choose a payment method</option>
+              <option value="cash">Cash</option>
+              <option value="fonepay">Fonepay</option>
+            </select>
+          </div>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleDialogClose}>Cancel</Button>
-          <Button onClick={handleDialogClose}>Save</Button>
+          <Button className="CashCancel" onClick={handleDialogClose}>Cancel</Button>
+          <Button className="CashSave" onClick={handleRedirect}>Continue</Button>
         </DialogActions>
       </Dialog>
     </div>
