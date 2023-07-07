e7745b85deaa6c0a9db622e2a3dcedec8ab3a4dd
rinabade <104953248+rinabade@users.noreply.github.com>
2023-07-07 16:28:16 +0545

    changes in kitchen and cashier

---
+++ client/src/components/Token/axiosinterceptor.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+// Add a request interceptor
+axios.interceptors.request.use(
+    config => {
+        const tokenString = sessionStorage.getItem('token');
+        const userToken = JSON.parse(tokenString);
+        const token = userToken?.token ? userToken.token : null
+        console.log("token--------", token)
+        if (token) {
+            config.headers['Authorization'] = 'Bearer ' + token
+        }
+        // config.headers['Content-Type'] = 'application/json';
+        return config
+    },
+    error => {
+        Promise.reject(error)
+    }
+)
